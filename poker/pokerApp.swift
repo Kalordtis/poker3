557eaea27f3af3087bab171e9423b54e487be628
sriponu <sriponu@ad.unc.edu>
2026-08-31 21:48:35 -0400

    Initial Commit

---
+++ poker/pokerApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokerApp.swift
+//  poker
+//
+//  Created by Srikar Ponugoti on 8/31/26.
+//
+
+import SwiftUI
+
+@main
+struct pokerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
